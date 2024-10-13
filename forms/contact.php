0f29320dbff102711f92de23120d678a8d22d482
Suhail Haroon <suhailharoon500@gmail.com>
2024-10-13 22:22:35 +0530

    added backend for sending form data as mail

--- forms/contact.php
+++ forms/contact.php
@@ -20,7 +20,7 @@
   
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
+  $contact->reply_to_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
   $contact->add_message( $_POST['name'], 'From');
